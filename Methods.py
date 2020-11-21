9f5da0f79a1e5393a1fdec28cef51a1305174015
Opa Towobola <67545758+CarveTheFuture@users.noreply.github.com>
2020-11-20 18:11:44 -0700

    Update Methods.py

--- Methods.py
+++ Methods.py
@@ -9,9 +9,3 @@ class MyClass:
     @staticmethod
     def staticmethod():
         return 'static method called'
-def Subtract (a, b): 
-    return (a-b) 
-  
-print( Subtract(10, 12) ) # prints -2 
-  
-print( Subtract(15, 6) ) # prints 9 
